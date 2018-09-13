27ca57e5080b00ce0b687043111ea4a1c01e7efb
AlgoBen <benzie2k@yahoo.com>
2018-09-13 23:37:49 +0800

    Add files via upload

---
+++ thread example.py
@@ -0,0 +1,14 @@
+import time, requests, threading
+
+def thread1():
+	while 1:
+		print("alpha")
+		time.sleep(1)
+
+def thread2():
+	while 1:
+		print("bravo")
+		time.sleep(1)
+
+threading.Thread(target = thread1,).start()
+threading.Thread(target = thread2,).start()
